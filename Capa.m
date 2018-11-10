2b7b4b0c12d69f613604ff0ed5818a9b019e571d
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2018-11-10 09:59:51 -0600

    Método para calcular error de una salida

--- Capa.m
+++ Capa.m
@@ -3,7 +3,7 @@
 classdef Capa < handle
     properties (Access = public)
         salidas %vector
-        pesos %matriz de pesos entre la ca
+        pesos %matriz de pesos entre la capa actual y la inferior
         errores %vector
         n %número de neuronas en la capa
     end
--- Red.m
+++ Red.m
@@ -1,19 +1,48 @@
-classdef Red
+% Heredar de la clase handle hace que el objeto se maneje por referencia
+% y no por copia.
+classdef Red < handle
     properties
+        numCapas %número de capas
+        numNeuronas %neuronas por capa
         capas %vector de capas
         eta %velocidad de aprendizaje
+        f = @(x) 1/(1 + exp(-x)); %función de salida
     end
     methods
-        function obj = Red(numCapas, neuronasPorCapa, velocidadAprendizaje)
+        % Constructor. Define la cantidad de capas, neuronas por capa
+        % y la velocidad de aprendizaje de la red.
+        function obj = Red(numeroCapas, neuronasPorCapa, velocidadAprendizaje)
+            obj.numCapas = numeroCapas;
+            obj.numNeuronas = neuronasPorCapa;
             obj.capas(numCapas) = obj.capas; %hack para reservar memoria
-            for i=1:numCapas
-                obj.capas(i) = Capa(neuronasPorCapa);
+            
+            % TODO: Revisar bien los tamaños de la capa de entrada y la
+            % capa de salida.
+            for ii=1:numCapas
+                obj.capas(ii) = Capa(numNeuronas);
             end
             obj.eta = velocidadAprendizaje;
         end
         
-        function propagar_capa(inferior, superior)
-            superior.salidas = superior.pesos * inferior.salidas;
+        %% Métodos de propagación hacia adelante
+        
+        % OJO: Matlab necesita como parámetro siempre el objeto mismo
+        % que tiene el método dentro.
+        % ver https://www.mathworks.com/help/matlab/matlab_oop/specifying-methods-and-functions.html
+        
+        % Propaga la señal de una capa a la siguiente.
+        function propagar_capa(red, inferior, superior)
+            superior.salidas = red.f(superior.pesos * inferior.salidas);
+        end
+        
+        %% Métodos de propagación hacia atrás
+        
+        % Calcula el error entre la salida de la red y el valor esperado
+        % para una entrada particular 
+        function calcular_error_salida(red, objetivo)
+            salidas = red.capas(red.numCapas).salidas;
+            red.capas(red.numCapas).errores = salidas .* (1 - salidas) ...
+                .* (objetivo - salidas);
         end
     end
 end
