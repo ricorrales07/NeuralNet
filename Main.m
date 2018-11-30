fea2069a77094976929b7d8aa67564c36ac0a8c6
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2018-11-30 11:48:47 -0600

    Main minimal

--- Main.m
+++ Main.m
@@ -1,16 +1,50 @@
 % <>
 
+red = Red(3, 0.1, 1000, 3072, 10);
+
+datos = 'C:\Users\Ricardo\Desktop\datasets_numerico\cifar-10-matlab.tar\cifar-10-matlab\cifar-10-batches-mat\');
+
+entrenarRed(red, datos, 0.01, 1);
+
+datosDePrueba = load(strcat(datosDeEntrenamiento, 'test_batch.mat'));
+
+categorias = ['avión';
+    'carro';
+    'pájaro';
+    'gato';
+    'venado';
+    'perro';
+    'rana';
+    'caballo';
+    'barco';
+    'camión'];
+
+s = '';
+while s ~= 'q'
+    s = input('Inserte número:');
+    [~,salida] = max(red.propagar_adelante(datosDePrueba.data(s,:)));
+    im = reshape(datosDePrueba.data(s,:), 32,32,3);
+    imshow(im);
+    title(categorias(salida));
+end
+
 % La idea es que datosDeEntrenamiento sea solo el nombre de la carpeta
 % o algo así, de donde se puedan ir sacando los datos uno por uno.
 % No se pasan todos de un solo porque se quedaría sin memoria.
-function entrenarRed(red, datosDeEntrenamiento, errorAceptable)
-    n = %número de vectores de entrenamiento
+function entrenarRed(red, datosDeEntrenamiento, errorAceptable, batches)
+    I = eye(10);
     error = Inf;
     while error >= errorAceptable
         error = 0;
-        for ii=1:n
-            [entrada, objetivo] = %obtener el dato de entrenamiento
-            error = max(error, red.entrenarUnaEntrada(entrada, objetivo);
+        for jj=1:batches
+            datos = load(strcat(datosDeEntrenamiento, 'data_batch_', ...
+                num2str(jj), '.mat'));
+            n = size(datos.data, 1);
+            for ii=1:n
+                entrada = datos.data(ii,:);
+                objetivo = I(:,datos.labels(ii)+1);
+                error = max(error, red.entrenarUnaEntrada(entrada, objetivo));
+            end
         end
     end
 end
--- pruebas.m
+++ pruebas.m
@@ -1,6 +1,19 @@
-y = objeto(1)
-hola(y)
-y
+[a] = loadpickle('C:\Users\Ricardo\Desktop\datasets_numerico\cifar-10-matlab.tar\cifar-10-matlab\cifar-10-batches-mat\test_batch.mat')
+
+
+%y = objeto(1)
+%hola(y)
+%y
 function hola(y)
     y.x = 0;
+end
+
+function [a] = loadpickle(filename)
+    if ~exist(filename,'file')
+        error('%s is not a file',filename);
+    end
+    outname = [tempname() '.mat'];
+    pyscript = ['import cPickle as pickle;import sys;import scipy.io;file=open("' filename '","r");dat=pickle.load(file);file.close();scipy.io.savemat("' outname '",dat)'];
+    system(['LD_LIBRARY_PATH=/opt/intel/composer_xe_2013/mkl/lib/intel64:/opt/intel/composer_xe_2013/lib/intel64;python -c ''' pyscript '''']);
+    a = load(outname);
 end
