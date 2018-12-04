a8f81b990749f66c3934da036998779c15d96119
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2018-12-04 10:35:25 -0600

    It's alive!

--- Main.m
+++ Main.m
@@ -5,7 +5,9 @@ red = Red(4, 0.05, 1000, 3072, 10);
 
 datos = 'C:\Users\Ricardo\Desktop\datasets_numerico\cifar-10-matlab.tar\cifar-10-matlab\cifar-10-batches-mat\';
 
-entrenarRed2(red, datos, 0.1, 1);
+[mu, sigma] = obtenerNormalizacion(datos);
+
+entrenarRed2(red, datos, 0.3, 1, mu, sigma);
 
 datosDePrueba = load(strcat(datos, 'test_batch.mat'));
 
@@ -24,12 +26,12 @@ s = input('Inserte número: ');
 I = eye(10);
 while num2str(s) ~= 'q'
     x = red.propagar_adelante(((double(datosDePrueba.data(s,:)) - mu) ./ sigma)');
-    error = 0.5 * sum((double(I(:,datosDePrueba.labels(s)+1)) - x).^2)
-    error2 = red.calcular_error_salida(double(I(:,datosDePrueba.labels(s)+1)))
+    errorRaw = 0.5 * sum((double(I(:,datosDePrueba.labels(s)+1)) - x).^2)
+    errorPrediccion = red.calcular_error_salida(double(I(:,datosDePrueba.labels(s)+1)))
     [~,salida] = max(x);
     im = reshape(datosDePrueba.data(s,:), 32,32,3);
     imshow(im);
-    title(strcat("Esperado: ", categorias(datosDePrueba.labels(s)+1), "; Obtenido: ", categorias(salida)));
+    title(strcat("Esperado: ", categorias(datosDePrueba.labels(s)+1), " Obtenido: ", categorias(salida)));
     s = input('Inserte número: ');
 end
 
@@ -67,22 +69,21 @@ function entrenarRed(red, datosDeEntrenamiento, errorAceptable, batches)
     end
 end
 
-function entrenarRed2(red, datosDeEntrenamiento, errorAceptable, batches)
+function entrenarRed2(red, datosDeEntrenamiento, errorAceptable, batches, mu, sigma)
     I = eye(10);
     conError = 1; % porcentaje de imágenes erróneamente clasificadas
     k = 0;
     while conError >= errorAceptable
         conError = 0;
+        totalEntradas = 0;
         for jj=1:batches
             datos = load(strcat(datosDeEntrenamiento, 'data_batch_', ...
                 num2str(jj), '.mat'));
             datos.data = double(datos.data);
             datos.labels = double(datos.labels);
             
-            mu = mean(datos.data(1:100,:));
-            sigma = var(datos.data(1:100,:));
-            
-            n = size(datos.data, 1) / 100;
+            n = size(datos.data, 1);
+            totalEntradas = totalEntradas + n;
             for ii=1:n
                 % Se aplica esta transformación a los datos de entrada para
                 % obtener cosas entre -1 y 1.
@@ -92,12 +93,34 @@ function entrenarRed2(red, datosDeEntrenamiento, errorAceptable, batches)
                 error1entrada = red.entrenarUnaEntrada(entrada, objetivo);
                 conError = conError + (error1entrada > 0);
                 if mod(ii, 100) == 0
-                    fprintf('Entrenando: %d imágenes de 10000\n', ii);
+                    fprintf('Entrenando: %d imágenes de %d (batch %d)\n', ...
+                        ii, n, jj);
                 end
             end
         end
-        conError = conError / (n * batches); %asumo que todos los batches tienen el mismo tamaño
+        conError = conError / totalEntradas;
         k = k + 1;
-        fprintf('Iteración: %d, error: %f\n', k, conError * 100);
+        fprintf('Iteración: %d, porcentaje de error: %f\n', ...
+            k, conError * 100);
     end
+end
+
+function [promedio, varianza] = obtenerNormalizacion(datosDeEntrenamiento)
+    mu = zeros(5,3072);
+    sigma = zeros(5,3072);
+    
+    for ii = 1:5
+        datos = load(strcat(datosDeEntrenamiento, 'data_batch_', ...
+            num2str(ii), '.mat'));
+        datos.data = double(datos.data);
+        datos.labels = double(datos.labels);
+
+        mu(ii,:) = mean(datos.data);
+        sigma(ii,:) = var(datos.data);      
+    end
+    
+    promedio = mean(mu);
+    
+    % Fórmula tomada de https://stats.stackexchange.com/questions/10441/how-to-calculate-the-variance-of-a-partition-of-variables
+    varianza = (9999 / 49999) * sum(sigma + (40000 / 9999) * var(mu));
 end
---
+++ Main2.m
@@ -0,0 +1,55 @@
+%DATASET: MNIST
+% <>
+
+clc
+red = Red(4, 0.05, 100, 784, 10);
+
+datos = 'C:\Users\Ricardo\Desktop\datasets_numerico\MNIST\';
+
+imgsEntrenamiento = loadMNISTImages(strcat(datos, 'train-images.idx3-ubyte'))';
+labelsEntrenamiento = loadMNISTLabels(strcat(datos, 'train-labels.idx1-ubyte'));
+
+% 30% de error aceptable
+entrenarRed(red, imgsEntrenamiento, labelsEntrenamiento, 0.3);
+
+imgsPrueba = loadMNISTImages(strcat(datos, 't10k-images.idx3-ubyte'));
+labelsPrueba = loadMNISTLabels(strcat(datos, 't10k-labels.idx1-ubyte'));
+
+s = input('Inserte número (1-6000): ');
+I = eye(10);
+while num2str(s) ~= 'q'
+    x = red.propagar_adelante(imgsPrueba(:,s));
+    errorRaw = 0.5 * sum((I(:,labelsPrueba(s)+1) - x).^2)
+    errorPrediccion = red.calcular_error_salida(I(:,labelsPrueba(s)+1))
+    [~,salida] = max(x);
+    im = reshape(imgsPrueba(:,s), 28, 28);
+    imshow(im);
+    title(strcat("Esperado: ", num2str(labelsPrueba(s)), " Obtenido: ", num2str(salida-1)));
+    s = input('Inserte número: ');
+end
+
+function entrenarRed(red, imgs, labels, errorAceptable)
+    I = eye(10);
+    conError = 1; % porcentaje de imágenes erróneamente clasificadas
+    k = 0;
+    while conError >= errorAceptable
+        conError = 0;
+            
+        n = size(imgs, 1);
+        for ii=1:n
+            entrada = imgs(ii,:)';
+            objetivo = I(:,labels(ii)+1);
+            error1entrada = red.entrenarUnaEntrada(entrada, objetivo);
+            conError = conError + (error1entrada > 0);
+            if mod(ii, 1000) == 0
+                fprintf('Entrenando: %d imágenes de %d\n', ...
+                    ii, n);
+            end
+        end
+        
+        conError = conError / n;
+        k = k + 1;
+        fprintf('Iteración: %d, porcentaje de error: %.2f\n', ...
+            k, conError * 100);
+    end
+end
