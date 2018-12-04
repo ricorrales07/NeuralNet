ab82be5ffb854376154985ff10f9fad51c66dbf6
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2018-12-04 12:50:47 -0600

    Redes entrenadas

--- Main2.m
+++ Main2.m
@@ -9,26 +9,30 @@ datos = 'C:\Users\Ricardo\Desktop\datasets_numerico\MNIST\';
 imgsEntrenamiento = loadMNISTImages(strcat(datos, 'train-images.idx3-ubyte'))';
 labelsEntrenamiento = loadMNISTLabels(strcat(datos, 'train-labels.idx1-ubyte'));
 
-% 30% de error aceptable
-entrenarRed(red, imgsEntrenamiento, labelsEntrenamiento, 0.3);
+% 1% de error aceptable
+[errorPorEntrada, errorPorIteracion] = entrenarRed(red, imgsEntrenamiento, labelsEntrenamiento, 0.01);
 
 imgsPrueba = loadMNISTImages(strcat(datos, 't10k-images.idx3-ubyte'));
 labelsPrueba = loadMNISTLabels(strcat(datos, 't10k-labels.idx1-ubyte'));
 
+%%
+
+evaluarErrorPrueba(red, imgsPrueba, labelsPrueba)
+
 s = input('Inserte número (1-6000): ');
 I = eye(10);
-while num2str(s) ~= 'q'
-    x = red.propagar_adelante(imgsPrueba(:,s));
+while s ~= -1
+    x = red.propagar_adelante(imgsPrueba(:,s))
     errorRaw = 0.5 * sum((I(:,labelsPrueba(s)+1) - x).^2)
     errorPrediccion = red.calcular_error_salida(I(:,labelsPrueba(s)+1))
     [~,salida] = max(x);
     im = reshape(imgsPrueba(:,s), 28, 28);
     imshow(im);
     title(strcat("Esperado: ", num2str(labelsPrueba(s)), " Obtenido: ", num2str(salida-1)));
-    s = input('Inserte número: ');
+    s = input('Inserte número (1-6000): ');
 end
 
-function entrenarRed(red, imgs, labels, errorAceptable)
+function [errorPorEntrada, errorPorIteracion] = entrenarRed(red, imgs, labels, errorAceptable)
     I = eye(10);
     conError = 1; % porcentaje de imágenes erróneamente clasificadas
     k = 0;
@@ -40,6 +44,7 @@ function entrenarRed(red, imgs, labels, errorAceptable)
             entrada = imgs(ii,:)';
             objetivo = I(:,labels(ii)+1);
             error1entrada = red.entrenarUnaEntrada(entrada, objetivo);
+            errorPorEntrada(k * n + ii) = error1entrada;
             conError = conError + (error1entrada > 0);
             if mod(ii, 1000) == 0
                 fprintf('Entrenando: %d imágenes de %d\n', ...
@@ -51,5 +56,31 @@ function entrenarRed(red, imgs, labels, errorAceptable)
         k = k + 1;
         fprintf('Iteración: %d, porcentaje de error: %.2f\n', ...
             k, conError * 100);
+        errorPorIteracion(k) = conError;
     end
+end
+
+function imagenesConError = evaluarErrorPrueba(red, imgs, labels)
+    I = eye(10);
+    conError = 0; % porcentaje de imágenes erróneamente clasificadas
+            
+    n = size(imgs, 1);
+    for ii=1:n
+        entrada = imgs(:,ii)';
+        objetivo = I(:,labels(ii)+1);
+        red.propagar_adelante(entrada);
+        error1entrada = red.calcular_error_salida(objetivo);
+        if error1entrada > 0
+            conError = conError + 1;
+            imagenesConError(conError) = ii;
+        end
+        if mod(ii, 1000) == 0
+            fprintf('Evaluando: %d imágenes de %d\n', ...
+                ii, n);
+        end
+    end
+        
+    conError = conError / n;
+    fprintf('Fin de pruebas. Porcentaje de error: %.2f\n', ...
+        conError * 100);
 end
